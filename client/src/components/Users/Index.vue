c4551d4111f7e9d9e895aaa0c2fa3749b0648c2e
Thirawut Chawala <wut@MacBook-Pro-khxng-Thirawut.local>
2019-03-21 01:30:39 +0700

    CRUD

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,13 +1,62 @@
 <template>
-    <h1>Get All Users</h1>
+    <div>
+        <h1>Get All Users</h1>
+        <h4>จำนวนผู้ใช้งาน: {{ users.length }}</h4>
+        <p><button v-on:click="navigateTo('/user/create')">เพิ่มผู้ใชางาน</button></p>
+        <div v-for="user in users" :key="user.id">
+            <div>id: {{ user.id }}</div>
+            <div>ชื่อ-นามสกุล: {{ user.name }} {{ user.lastname }}</div>
+            <div>Email: {{ user.email }}</div>
+            <div>Password: {{ user.password }}</div>
+            <p>
+                <button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button>
+                <button v-on:click="navigateTo('/user/edit/'+user.id)">แก้ไขข้อมูล</button>
+                <button v-on:click="deleteUser(user)">ลบผู้ใช้</button>
+            </p>
+            <hr>
+        </div>
+    </div>
 </template>
 
 <script>
+import UsersService from '@/services/UsersService'
+
 export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    async created () {
+        try {
+            this.users = (await UsersService.index()).data
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    methods: {
+        navigateTo(route) {
+            this.$router.push(route)
+        }, 
+        async deleteUser(user) {
+            let result = confirm("Want to delete?")
+            if (result) {
+                try {
+                    await UsersService.delete(user)
+                    this.refreshData()
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        },
+        async refreshData() {
+            this.users = (await UsersService.index()).data
+        }
+    }
 
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- client/src/components/Users/ShowUser.vue
+++ client/src/components/Users/ShowUser.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>Get User By id</h1>
+    <div>
+        <h1>Get User By id</h1>
+        <p>User ID: {{ user.id }}</p>
+        <p>ชือ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+        <p>email: {{ user.email }}</p>
+        <p>password: {{ user.password }}</p>
+    </div>
 </template>
 
 <script>
-export default {
+import UsersService from '@/services/UsersService'
 
+export default {
+    data() {
+        return {
+            user: null
+        }
+    },
+    async created() {
+        try {
+            let userId = this.$route.params.userId
+            this.user = (await UsersService.show(userId)).data
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 </script>
 
